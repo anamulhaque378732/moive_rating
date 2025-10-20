f4a684d6edd79fc6d3d084bdc095d219ef53ed58
Anamul Haque <anamulhaque4631@gmail.com>
2025-10-20 22:56:45 +0600

    moive rating projrct

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="text-6xl">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, hic. Aut animi corporis amet inventore voluptatum, veritatis quo nisi? Laudantium numquam harum eligendi ipsa unde, veritatis autem iste ipsam inventore.
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: "1.25rem"
+            },
+            color: {
+                primary: "#00D991"
+            }
+        },
+    },
+    plugin: [],
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
